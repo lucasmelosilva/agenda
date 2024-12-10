6a86904fe80c0126c3a56ba36afe0aea685fcdcc
Lucas Melo <lucas.melo044@gmail.com>
2024-12-10 18:09:24 -0300

    create utils to genarete contatics

--- contact/models.py
+++ contact/models.py
@@ -5,7 +5,7 @@ which represents a contact in the application.
 
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -13,6 +13,10 @@ class Category(models.Model):
     """The `Category` model represents a category in the application.
     It has a `name` field, which is a `CharField` with a maximum length
     of 50 characters."""
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
@@ -48,6 +52,11 @@ class Contact(models.Model):
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m')
     category = models.ForeignKey(
         Category,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
+    owern = models.ForeignKey(
+        User,
         on_delete=models.SET_NULL,
         blank=True, null=True
     )
