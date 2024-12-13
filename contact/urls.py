0c806f350b258d48fc5986b3aebfa54eb9b8e635
Lucas Melo <lucas.melo044@gmail.com>
2024-12-13 16:56:06 -0300

    create contact route

--- contact/urls.py
+++ contact/urls.py
@@ -3,6 +3,9 @@
 from django.urls import path
 from contact import views
 
+app_name = 'contact'
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path('<int:contact_id>/', views.contact, name='contact'),
+    path('', views.index, name='index'),
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -2,7 +2,7 @@
 This module contains the views for the contact application.
 """
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
 
 
@@ -17,5 +17,21 @@ def index(request):
     return render(
         request,
         'contact/index.html',
+        context,
+    )
+
+
+def contact(request, contact_id):
+    """Renders the contact.html template for the contact application."""
+    # result = Contact.objects.get(pk=contact_id)
+    result = get_object_or_404(Contact, pk=contact_id, show=True)
+
+    context = {
+        'contact': result
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
         context,
     )
