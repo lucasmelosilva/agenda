90ec2b3179cc3eb5e7a63b66966d1109ba702951
Lucas Melo <lucas.melo044@gmail.com>
2024-12-13 03:34:05 -0300

    create table template

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -3,12 +3,18 @@ This module contains the views for the contact application.
 """
 
 from django.shortcuts import render
+from contact.models import Contact
 
 
 def index(request):
     """Renders the index.html template for the contact application."""
+    contacts = Contact.objects.all()
+    context = {
+        'contacts': contacts
+    }
 
     return render(
         request,
-        'contact/index.html'
+        'contact/index.html',
+        context,
     )
