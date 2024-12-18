a9556392ad742a52b75572f4bc0076be2ba55771
Lucas Melo <lucas.melo044@gmail.com>
2024-12-18 19:38:12 -0300

    save datas

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,16 +1,21 @@
 """
 This module contains the views for the contact application.
 """
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from contact.forms import ContactForm
 
 
 def create(request):
     """Renders the create.html template for the contact application."""
     if request.method == 'POST':
+        form = ContactForm(request.POST)
         context = {
-            'form': ContactForm(request.POST)
+            'form': form
         }
+
+        if form.is_valid():
+            form.save()
+            return redirect('contact:create')
 
         return render(
             request,
