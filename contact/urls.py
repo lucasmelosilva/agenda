0a9180e813738ac85bf0e23fa366fe86a55170cd
Lucas Melo <lucas.melo044@gmail.com>
2024-12-19 19:27:56 -0300

    add delete url

--- contact/urls.py
+++ contact/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
     path('contact/create', views.create, name='create'),
     path('contact/<int:contact_id>/upadate/', views.update, name='update'),
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
 ]
